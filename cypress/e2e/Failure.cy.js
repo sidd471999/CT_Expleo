a03107e49b2b366395d667d79d8dccf1503badd1
Siddhant Arjun Gadakh <87557896+sidd471999@users.noreply.github.com>
2024-03-04 11:36:07 +0530

    Create Failure.cy.js

---
+++ cypress/e2e/Failure.cy.js
@@ -0,0 +1,14 @@
+import { login } from "../support/login"
+
+describe('Demo Test', () => {  // test suite
+  it('Login With correct Credentrails', () => {  // testcases
+
+    //visit the website
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //Login into Account
+    login.LoginIntoOrangeHRM("User2")
+    //verify dashboard text
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains("Dashboard")
+  })
+  
+})
